e800df9220d3e17fd32138ade312c2395b959f3e
samlui1009 <sam_lui@hotmail.ca>
2025-08-19 14:25:52 -0700

    Added navigation links from Home page

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import './NavBar.css';
+import { useNavigate } from 'react-router-dom';
+
+function NavBar() {
+
+    const navigate = useNavigate();
+
+    const goTBWatched = () => {
+        navigate('/to-be-watched')
+    }
+
+    const goCurrentlyWatching = () => {
+        navigate('/currently-watching')
+    }
+
+    const goWatched = () => {
+        navigate('/watched')
+    }
+
+    const goDropped = () => {
+        navigate('/dropped')
+    }
+
+    const goSTiers = () => {
+        navigate('/s-tiers')
+    }
+
+    return (
+        <div className="nav-container">
+            <ul className="nav-bar">
+                <button className="btn" onClick={goTBWatched}><li>To Watch</li></button>
+                <button className="btn" onClick={goCurrentlyWatching}><li>Currently Watching</li></button>
+                <button className="btn" onClick={goWatched}><li>Watched</li></button>
+                <button className="btn" onClick={goDropped}><li>Dropped</li></button>
+                <button className="btn" onClick={goSTiers}><li>S-Tiers</li></button>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
