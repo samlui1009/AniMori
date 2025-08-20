b651f2e2887cb6e2626c5c4d1ed5c75ce7b3675b
samlui1009 <sam_lui@hotmail.ca>
2025-08-19 21:54:05 -0700

    Set up for SQLite database to locally store animes now complete

--- src/main.js
+++ src/main.js
@@ -60,3 +60,6 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+// SQLite - Best for small desktop applications, lightweight 
+// https://www.youtube.com/watch?v=nMvjcBTFlPA
